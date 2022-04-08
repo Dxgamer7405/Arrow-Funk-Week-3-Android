fc37e1a5823c13a220bb5eeb57572fc21ff7c6a4
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-08 00:46:57 -0300

    update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -41,6 +41,10 @@ class GameOverSubstate extends MusicBeatSubstate
 		instance = this;
 		PlayState.instance.callOnLuas('onGameOverStart', []);
 
+    #if android
+	  addVirtualPad(FULL, A_B);
+    #end
+
 		super.create();
 	}
 
