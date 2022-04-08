8754575d072443e8e2477c2901ec84d5d29de54a
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-08 00:47:21 -0300

    update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -64,6 +64,10 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+
 		#if MODS_ALLOWED
 		// Just to load a mod on start up if ya got one. For mods that change the menu music and bg
 		if (FileSystem.exists("modsList.txt")){
