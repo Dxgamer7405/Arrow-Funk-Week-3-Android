e0e71b2bfec378434cb5316a7203a7280ae0a0a2
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-08 00:57:43 -0300

    update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,7 +1,9 @@
 package;
 
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
+#end
 
 #if LUA_ALLOWED
 import llua.Lua;
@@ -12,11 +14,13 @@ using StringTools;
 
 class DiscordClient
 {
+	public static var isInitialized:Bool = false;
 	public function new()
 	{
+                #if desktop
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "960982295941103626",
+			clientID: "863222024192262205",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -31,21 +35,26 @@ class DiscordClient
 		}
 
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	public static function shutdown()
 	{
+                #if desktop
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	static function onReady()
 	{
+                #if desktop
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
 			largeImageText: "Psych Engine"
 		});
+                #end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -60,15 +69,19 @@ class DiscordClient
 
 	public static function initialize()
 	{
+                #if desktop
 		var DiscordDaemon = sys.thread.Thread.create(() ->
 		{
 			new DiscordClient();
 		});
 		trace("Discord Client initialized");
+		isInitialized = true;
+                #end
 	}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
 	{
+                #if desktop
 		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
 
 		if (endTimestamp > 0)
@@ -84,8 +97,9 @@ class DiscordClient
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
+                #end
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
