bd993febd3e55a3404008ef4ca9ee687439ceb3d
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-08 09:12:07 -0300

    update NotesSubState.hx

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -91,6 +91,10 @@ class NotesSubState extends MusicBeatSubstate
 		add(hsbText);
 
 		changeSelection();
+
+                #if android
+                addVirtualPad(FULL, A_B_C);
+                #end
 	}
 
 	var changingNote:Bool = false;
@@ -103,7 +107,7 @@ class NotesSubState extends MusicBeatSubstate
 				} else if(controls.UI_RIGHT_P) {
 					updateValue(1);
 					FlxG.sound.play(Paths.sound('scrollMenu'));
-				} else if(controls.RESET) {
+				} else if(controls.RESET #if android || _virtualpad.buttonC.justPressed #end) {
 					resetValue(curSelected, typeSelected);
 					FlxG.sound.play(Paths.sound('scrollMenu'));
 				}
@@ -144,6 +148,12 @@ class NotesSubState extends MusicBeatSubstate
 				changeType(1);
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
+			if(controls.RESET #if android || _virtualpad.buttonC.justPressed #end) {
+				for (i in 0...3) {
+					resetValue(curSelected, i);
+				}
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+			}
 			if(controls.RESET) {
 				for (i in 0...3) {
 					resetValue(curSelected, i);
@@ -175,7 +185,12 @@ class NotesSubState extends MusicBeatSubstate
 
 		if (controls.BACK || (changingNote && controls.ACCEPT)) {
 			if(!changingNote) {
-				close();
+			        #if android
+                                FlxTransitionableState.skipNextTransOut = true;
+			        FlxG.resetState();
+                                #else
+                                close();
+                                #end
 			} else {
 				changeSelection();
 			}
