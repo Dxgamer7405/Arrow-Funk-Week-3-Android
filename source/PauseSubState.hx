035007fdb71d8d905d6bac6de4c175398c998328
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-08 00:47:09 -0300

    update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -133,6 +133,18 @@ class PauseSubState extends MusicBeatSubstate
 		changeSelection();
 
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+
+                #if android
+                if (PlayState.chartingMode)
+                {
+                        addVirtualPad(FULL, A);
+                }
+                else
+                {
+                        addVirtualPad(UP_DOWN, A);
+                }
+                addPadCamera();
+                #end
 	}
 
 	override function update(elapsed:Float)
