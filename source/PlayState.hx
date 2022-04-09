d9f27a5a0af6032bd283880453402f0922ed2b95
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-09 12:32:55 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4369,13 +4369,6 @@ class PlayState extends MusicBeatState
 		var right = controls.NOTE_RIGHT;
 		var down = controls.NOTE_DOWN;
 		var left = controls.NOTE_LEFT;
-
-		var upP = controls.NOTE_UP_P;
-		var rightP = controls.NOTE_RIGHT_P;
-		var downP = controls.NOTE_DOWN_P;
-		var leftP = controls.NOTE_LEFT_P;
-
-		var controlArray:Array<Bool> = [leftP, downP, upP, rightP];
 		var controlHoldArray:Array<Bool> = [left, down, up, right];
 		
 		var controlArray:Array<Bool> = [controls.NOTE_LEFT_P, controls.NOTE_DOWN_P, controls.NOTE_UP_P, controls.NOTE_RIGHT_P];
